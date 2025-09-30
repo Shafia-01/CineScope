eeed66e899a64451591bd1fc00329f90d66a40ef
Shafia-01 <shafiaameeruddin637@gmail.com>
2025-10-01 00:46:19 +0530

    updated

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = "https://cinescope-7k1y.onrender.com";
 
 const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
